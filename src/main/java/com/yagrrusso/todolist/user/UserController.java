b687eff0b679faa58590d52ebcffbedb68ab27c2
Yago Russo <yagrrusso@gmail.com>
2023-10-13 09:05:27 -0300

    feat: user create and list

---
+++ src/main/java/com/yagrrusso/todolist/user/UserController.java
@@ -0,0 +1,37 @@
+package com.yagrrusso.todolist.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired()
+    private IUserRepository userRepository;
+
+    @GetMapping()
+    public List<UserModel> list() {
+        List<UserModel> usersList = this.userRepository.findAll();
+        return usersList;
+    }
+
+    @PostMapping()
+    public ResponseEntity create(@RequestBody() UserModel user) {
+        var username = this.userRepository.findByUsername(user.getUsername());
+        if (username != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("user already created");
+        }
+
+        var createdUser = this.userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+}
